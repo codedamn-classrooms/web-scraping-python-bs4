85bf26e6bb097096eb5c5b606bed40fc7a74fee8
Mehul Mohan <mehulmpt@gmail.com>
2020-09-24 04:07:29 +0530

    lab

---
+++ script.py
@@ -0,0 +1,6 @@
+import requests
+
+# Make a request to https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/
+# Store the result in 'res' variable
+
+# print the result
