e51d0a1c330bd8a0c3c76439e2d478eba56ddb07
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 03:15:25 +0530

    lab

--- script.py
+++ script.py
@@ -6,8 +6,10 @@ page = requests.get(
     "https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Extract title of page
-page_title = soup.title.text
+# Extract info of page
+page_title = ''
+page_body = ''
+page_head = ''
 
 # print the result
-print(page_title)
+print(page_title, page_body, page_head)
