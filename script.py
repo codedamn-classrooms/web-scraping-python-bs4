4c3779300fcc83d3c716b1ba86ec8e79269f8bdf
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 01:50:18 +0530

    lab

--- script.py
+++ script.py
@@ -2,5 +2,10 @@ import requests
 
 # Make a request to https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/
 # Store the result in 'res' variable
+res = requests.get(
+    'https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/')
+txt = res.text
+status = res.status_code
 
+print(txt, status)
 # print the result
