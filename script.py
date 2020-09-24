c267798baf118c7e3f2c3aa498f07ff01e18b8fd
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 03:01:14 +0530

    lab

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 # Make a request to https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/
+page = requests.get(
+    "https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/")
+soup = BeautifulSoup(page.content, 'html.parser')
 
 # Extract title of page
-page_title = '<replace with title>'
+page_title = soup.title
 
 # print the result
 print(page_title)
