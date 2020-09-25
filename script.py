afa8d6f3738638b32dcfa416a2bbefdc2cf76e28
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 20:09:24 +0530

    lab

--- script.py
+++ script.py
@@ -11,11 +11,11 @@ top_items = []
 # Extract and store in top_items according to instructions on the left
 products = soup.select('div.thumbnail')
 for elem in products:
-    title = elem.select('h4 > a.title').text
-    review_label = elem.select('div.ratings').text
+    title = elem.select('h4 > a.title')[0].text
+    review_label = elem.select('div.ratings')[0].text
     info = {
-        "title": title,
-        "review": review_label
+        "title": title.strip(),
+        "review": review_label.strip()
     }
     top_items.append(info)
 
