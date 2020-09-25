c9a3fe39afd58e8cd8078022bcdf0c5d2172e9f0
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 21:30:29 +0530

    lab

--- script.py
+++ script.py
@@ -1,21 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 # Make a request
 page = requests.get(
     "https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # Create top_items as empty list
-all_links = []
+all_products = []
 
 # Extract and store in top_items according to instructions on the left
-links = soup.select('a')
-for ahref in links:
-    text = ahref.text
-    text = text.strip() if text is not None else ''
+products = soup.select('div.thumbnail')
+for product in products:
+    name = product.select('h4 > a')[0].text.strip()
+    description = product.select('p.description')[0].text.strip()
+    price = product.select('h4.price')[0].text.strip()
+    reviews = product.select('div.ratings')[0].text.strip()
+    image = product.select('img')[0].get('src')
 
-    href = ahref.get('href')
-    href = href.strip() if href is not None else ''
-    all_links.append({"href": href, "text": text})
+    all_products.append({
+        "name": name,
+        "description": description,
+        "price": price,
+        "reviews": reviews,
+        "image": image
+    })
 
-print(all_links)
+
+keys = all_products[0].keys()
+
+with open('products.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(all_products)
