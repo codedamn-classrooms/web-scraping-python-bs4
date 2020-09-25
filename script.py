9e479905db7362eccd1ed303bdad98723673bd0c
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 19:58:22 +0530

    lab

--- script.py
+++ script.py
@@ -5,14 +5,7 @@ page = requests.get(
     "https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Create all_h1_tags as empty list
-all_h1_tags = []
+# Create top_items as empty list
+top_items = []
 
-# Set all_h1_tags to all h1 tags of the soup
-for element in soup.select('h1'):
-    all_h1_tags.append(element.text)
-
-# Create seventh_p_text and set it to 7th p element text of the page
-seventh_p_text = soup.select('p')[6].text
-
-print(all_h1_tags, seventh_p_text)
+# Extract and store in top_items according to instructions on the left
