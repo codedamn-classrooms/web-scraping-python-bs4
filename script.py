f90a9f136ce3d667ac38573cb80c1a0f5e10e5e2
Mehul Mohan <mehulmpt@gmail.com>
2020-09-25 19:34:09 +0530

    lab

--- script.py
+++ script.py
@@ -6,7 +6,13 @@ page = requests.get(
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # Create all_h1_tags as empty list
+all_h1_tags = []
 
 # Set all_h1_tags to all h1 tags of the soup
+for element in soup.select('h1'):
+    all_h1_tags.append(element.text)
 
 # Create seventh_p_text and set it to 7th p element text of the page
+seventh_p_text = soup.select('p')[6].text
+
+print(all_h1_tags, seventh_p_text)
